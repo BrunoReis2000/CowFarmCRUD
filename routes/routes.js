ccc8c0ebe72874d7a5dc81861bc49531fba0a508
Bruno <bruno1.ascencao@gmail.com>
2025-11-01 20:34:56 +0000

    Replaces homepage with cow list table UI
    Transitions from a navbar-focused layout to a responsive table
    displaying cow data. Streamlines the interface for quicker
    data access and future management actions. Prepares codebase
    for CRUD operations by introducing action icons.

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-router.get('/cows', (req, res) => {
-    res.send('List of cows');
-});
+
 
 router.get('/', (req, res) => {
     res.render('index', { title: 'Home Page' });
 });
+
 
 module.exports = router;
