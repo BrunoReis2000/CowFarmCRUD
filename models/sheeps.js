932f6d5ac0cb90fd9d5a8474310ab17f2a7086cd
BrunoReis2000 <119542686+BrunoReis2000@users.noreply.github.com>
2026-01-20 16:00:56 +0000

    Remove uniqueness constraint from checkDigit field

--- models/sheeps.js
+++ models/sheeps.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const sheepSchema = new mongoose.Schema({
     tag: {type: Number, required: true, unique: true},
-    checkDigit: {type: Number, required: true, unique: true},
+    checkDigit: {type: Number, required: true},
     breedCount: {type: Number, required: false},
     lastTimeCalved: {type: Date, required: false},
     dateOfLastSanitation: {type: Date, required: false},
